5e7739306ec4b011e03ca6fab29f7a264302790f
BurningHat12 <yashgohel395@gmail.com>
2024-04-28 00:31:36 +0530

    Update Nav component and Hero section

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,7 +20,7 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg:block">
+        <div className="hidden max-lg:block ">
           <img src={hamburger} alt="Hamburger" width={25} height={25} />
         </div>
       </nav>
