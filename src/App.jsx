ecbb34b6521cf376f002a7058a0c3a67bb9e6ad0
BurningHat12 <yashgohel395@gmail.com>
2024-04-29 23:44:05 +0530

    Update CustomCursor component styles and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import CustomCursor from "./components/CustomCursor";
 import Nav from "./components/Nav";
+import CustomCursor from "./components/CustomCursor";
 import {
   CustomerReviews,
   Footer,
