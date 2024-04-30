83b6ac4af3cc8422f8b8899af5351b6a60a92516
BurningHat12 <yashgohel395@gmail.com>
2024-05-01 00:30:42 +0530

    Update CustomCursor component styles and index.css

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,30 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { arrowRight } from "../assets/icons";
 import Button from "../components/Button";
 import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
 
-import { useState } from "react";
-import ShoeCard from "../components/ShoeCard";
 import { motion } from "framer-motion";
-
-// need to add mix-blend mode to the image
+import CustomCursor from "../components/CustomCursor";
+import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
   const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
+  const handleMouseEnter = () => {
+    document.querySelector(".custom-cursor").classList.add("active");
+  };
+
+  const handleMouseLeave = () => {
+    document.querySelector(".custom-cursor").classList.remove("active");
+  };
 
   return (
     <section
       id="home"
       className="w-full  flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      <div
+        className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+      >
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collection
         </p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-xm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
-            The New Arriaval
+            The New Arrival
           </span>
           <br />
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
@@ -45,7 +55,6 @@ const Hero = () => {
           ))}
         </div>
       </div>
-
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <motion.img
           key={bigShoeImg} // Add a unique key for motion to track changes
