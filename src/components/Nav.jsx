be3a0c980e441fc93f681699d39e5ba4a5594c1c
BurningHat12 <yashgohel395@gmail.com>
2024-05-04 17:57:42 +0530

    Added motion animations to Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
 import { navLinks } from "../constants";
@@ -5,38 +6,56 @@ import { arrowRight } from "../assets/icons";
 
 const Nav = () => {
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
+    <motion.header className="padding-x py-8 absolute z-10 w-full">
       <nav className="flex justify-between items-center max-container">
-        <a href="/">
+        <motion.a
+          href="/"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
           <img src={headerLogo} alt="Logo" width={130} height={29} />
-        </a>
+        </motion.a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
-            <li key={item.label}>
+            <motion.li
+              key={item.label}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
               <a
                 href={item.href}
                 className="font-montserrat leading-normal text-lg text-slate-gray"
               >
                 {item.label}
               </a>
-            </li>
+            </motion.li>
           ))}
         </ul>
         <div className="max-lg:hidden flex gap-10">
-          <button className="flex justify-center items-center gap-2 px-7 py-3 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
+          <motion.button
+            className="flex justify-center items-center gap-2 px-7 py-3 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
             Sign In
-            <img
+            <motion.img
               src={arrowRight}
               alt="arrow right icon"
-              className="ml-2 rounded-full w-5 h-5 "
+              className="ml-2 rounded-full w-5 h-5"
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
             />
-          </button>
+          </motion.button>
         </div>
-        <div className="hidden max-lg:block ">
+        <motion.div
+          className="hidden max-lg:block"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
           <img src={hamburger} alt="Hamburger" width={25} height={25} />
-        </div>
+        </motion.div>
       </nav>
-    </header>
+    </motion.header>
   );
 };
 
